86122c3407b8ad11990ce87ca4c8f863b81c2baf
Zhiyi Wang <zhiyiwang2497@gmail.com>
2026-06-28 12:07:26 +0000

    remove unused url_for from routes.py

--- service/routes.py
+++ service/routes.py
@@ -21,7 +21,7 @@ This service implements a REST API that allows you to Create, Read, Update
 and Delete Wishlist
 """
 
-from flask import jsonify, request, url_for, abort
+from flask import jsonify, request, abort
 from flask import current_app as app  # Import Flask application
 from service.models import Wishlist, Item
 from service.common import status  # HTTP Status Codes
